666b79f569013bff16583bd4d0d70b2fb840cf2d
henry <henryzwei@icloud.com>
2026-04-16 18:32:58 +0200

    Initial Commit

---
+++ AutoSpotter/AutoSpotterApp.swift
@@ -0,0 +1,32 @@
+//
+//  AutoSpotterApp.swift
+//  AutoSpotter
+//
+//  Created by Henry on 16.04.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AutoSpotterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
